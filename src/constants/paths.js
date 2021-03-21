16e4749e0c22fd1a70197e0432b6f44b19faf232
28JayG <jaygandhi.32@outlook.com>
2021-03-21 17:28:58 +0530

    adds emprty cart screen

---
+++ src/constants/paths.js
@@ -0,0 +1,1 @@
+export const EMPTY_CART_IMAGE_URL = "https://i.ibb.co/JH49JHd/Closed-Stores-rafiki-1.png";
--- src/pages/purchase/purchase.component.jsx
+++ src/pages/purchase/purchase.component.jsx
@@ -1,19 +1,32 @@
 import React from "react";
+import { connect } from "react-redux";
 
 import Cart from "../../components/cart/cart.component";
 import Stepper from "../../components/stepper/stepper.component";
+import { EMPTY_CART_IMAGE_URL } from "../../constants/paths";
 
 import "./purchase.styles.scss";
 
-const Purchase = () => {
-  return (
+const Purchase = ({ itemsCount }) => {
+  return itemsCount > 0 ? (
     <section className="purchase">
       <div className="order-steps">
         <Stepper />
       </div>
       <Cart />
     </section>
+  ) : (
+    <div className="empty-cart">
+      <div className="empty-cart-image">
+        <img src={EMPTY_CART_IMAGE_URL} alt="" />
+      </div>
+      <h1>Your Cart seems Empty</h1>
+    </div>
   );
 };
 
-export default Purchase;
+const mapStateToProps = ({ cart }) => ({
+  itemsCount: cart.itemsCount,
+});
+
+export default connect(mapStateToProps)(Purchase);
